ee20bad324cef935f32f11c3f086ce62d0165f20
Aamir Sohail KmAs <AamirSohailKmAs@gmail.com>
2023-01-21 20:09:27 +0500

    fix: failing test

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,3 +4,11 @@ use Illuminate\Foundation\Testing\Concerns\InteractsWithConsole;
 use Porifa\ObjectBlueprintGenerator\Tests\TestCase;
 
 uses(TestCase::class, InteractsWithConsole::class)->in(__DIR__);
+
+uses()->afterEach(function () {
+    $configPath = app_path('Actions\FooAction.php');
+
+    if (file_exists($configPath)) {
+        unlink($configPath);
+    }
+})->in(__DIR__);
